1942c4b2d2313727f0c48970ccf2997ac4fbc67a
elene277 <elelopirtskhalava@gmail.com>
2024-11-30 16:11:58 +0400

    commit

--- Day 23/homework/homework.py
+++ Day 23/homework/homework.py
@@ -9,5 +9,7 @@ index = word.find("l")
 print(index)
 
 print("hello".find("l"))
+#we get this result because find function tells us where l is we typed l in find 
+#and it found l it is on 2 
 
 
---
+++ Day 24/classwork/classwork.py
@@ -0,0 +1,15 @@
+
+numbers = [1, 2, 3, 4, 5]
+
+
+product = 1
+
+
+for num in numbers:
+    product *= num
+
+
+print("სიის ელემენტების ნამრავლი არის: " + str(product))
+
+# Slicing არის მონაცემების შერჩევის პროცესი.
+
